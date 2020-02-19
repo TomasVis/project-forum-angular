60bad8bf2e9c254d84c9bc8f1b522a80eebea116
TomasVis <38735601+TomasVis@users.noreply.github.com>
2020-02-19 14:59:51 +0200

    Initial commit

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ArticleComponent } from './core/article/article.component';
+import { NavigationComponent } from './core/navigation/navigation.component';
+import { MainComponent } from './core/main/main.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { LeftSideComponent } from './core/left-side/left-side.component';
+import { RightSideComponent } from './core/right-side/right-side.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ArticleComponent,
+    NavigationComponent,
+    MainComponent,
+    FooterComponent,
+    LeftSideComponent,
+    RightSideComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
