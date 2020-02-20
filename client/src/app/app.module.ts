d7c16177c2fb22b02291837db8462ab3f86dabf8
TomasVis <38735601+TomasVis@users.noreply.github.com>
2020-02-20 10:29:26 +0200

    new folder structure

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,32 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ArticleComponent } from './core/article/article.component';
-import { NavigationComponent } from './core/navigation/navigation.component';
-import { MainComponent } from './core/main/main.component';
-import { FooterComponent } from './core/footer/footer.component';
-import { LeftSideComponent } from './core/left-side/left-side.component';
-import { RightSideComponent } from './core/right-side/right-side.component';
-import { ArticleTagsComponent } from './core/article-tags/article-tags.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { MainComponent } from './components/main/main.component';
+import { LeftSideComponent } from './components/left-side/left-side.component';
+import { RightSideComponent } from './components/right-side/right-side.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ArticleComponent } from './components/article/article.component';
+import { ArticleTagsComponent } from './components/article-tags/article-tags.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ArticleComponent,
     NavigationComponent,
     MainComponent,
-    FooterComponent,
     LeftSideComponent,
     RightSideComponent,
-    ArticleTagsComponent
+    FooterComponent,
+    ArticleComponent,
+    ArticleTagsComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
